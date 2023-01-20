23b1f42201c7e920b15c38785425ce29096c3b9c
Lindlar7 <75728800+xiahongchi@users.noreply.github.com>
2023-01-20 13:53:21 +0800

    Add files via upload

---
+++ lib/model/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+from .MLP import MLP
+from .HGPIFuMRNet import HGPIFuMRNet
+from .HGPIFuNetwNML import HGPIFuNetwNML
+from .ResBlkPIFuNet import ResBlkPIFuNet
+from .HGResBlkPIFuNet import HGResBlkPIFuNet
